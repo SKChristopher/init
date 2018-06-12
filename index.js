ff68530b3c2ce2cd57ca0ad92deb093b4e6caf71
Christopher <chris132128@gmail.com>
2018-06-11 20:21:13 -0700

    auto generate git ignore

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
+
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
 const files = require("./lib/files");
-const github = require('./lib/github');
+const github = require("./lib/github");
 
 clear();
 console.log(
@@ -17,12 +19,59 @@ if (files.directoryExists(".git")) {
 const inquirer = require("./lib/inquirer");
 
 const run = async () => {
-  let token = github.getStoredGithubToken();
-  if (!token) {
-    await github.setGithubCredentials();
-    token = await github.registerNewToken();
+  try {
+    // Retrieve & Set Authentication Token
+    const token = await getGithubToken();
+    github.githubAuth(token);
+
+    // Create remote repository
+    const url = await repo.createRemoteRepo();
+
+    // Create .gitignore file
+    await repo.createGitignore();
+
+    // Set up local repository and push to remote
+    const done = await repo.setupRepo(url);
+    if (done) {
+      console.log(chalk.green("All done!"));
+    }
+  } catch (err) {
+    if (err) {
+      switch (err.code) {
+        case 401:
+          console.log(
+            chalk.red(
+              "Couldn't log you in. Please provide correct credentials/token."
+            )
+          );
+          break;
+        case 422:
+          console.log(
+            chalk.red(
+              "There already exists a remote repository with the same name"
+            )
+          );
+          break;
+        default:
+          console.log(err);
+      }
+    }
   }
-  console.log(token);
+};
+
+const getGithubToken = async () => {
+  // Fetch token from config store
+  let token = github.getStoredGithubToken();
+  if (token) {
+    return token;
+  }
+
+  // No token found, use credentials to access GitHub account
+  await github.setGithubCredentials();
+
+  // register new token
+  token = await github.registerNewToken();
+  return token;
 };
 
 run();
